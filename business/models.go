c785904eb49914292c4ae8c279fa5d1c45e8f22c
Ahmed Alwahib <aalwheep@gmail.com>
2024-01-26 22:22:58 +0300

    generic type energy

--- business/models.go
+++ business/models.go
@@ -33,13 +33,13 @@ func (w *Wind) Print() string {
 
 // PrintGeneric returns any type as string.
 // The string is enriched with the required Kineteco legal information.
-func PrintGeneric[T any](t T) string {
+func PrintGeneric[T Energy](t T) string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, t)
 }
 
 // PrintSlice prints a slice of any type to the standard output.
 // Each item is enriched with its position and the Kineteco specific string.
-func PrintSlice[T any](tt []T) {
+func PrintSlice[T Energy](tt []T) {
 	for i, t := range tt {
 		fmt.Printf("%d: %s\n", i, PrintGeneric[T](t))
 	}
