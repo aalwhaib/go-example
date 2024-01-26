f9ccef040dcd2ccf224f8c64c18b11e83f04c972
Ahmed Alwahib <aalwheep@gmail.com>
2024-01-26 22:19:39 +0300

    init

---
+++ hello/hello.go
@@ -0,0 +1,19 @@
+package hello
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+
+	// // Create three different energy offers of kineteco
+	// solar2k := business.Solar{Name: "Solar 2000", Netto: 4.500}
+	// solar3k := business.Solar{Name: "Solar 3000", Netto: 4.000}
+	// windwest := business.Wind{Name: "Wind West", Netto: 3.950}
+
+	// // Print details for each energy offer with kineteco branding
+	// fmt.Println(solar3k.Print())
+	// fmt.Println(solar2k.Print())
+	// fmt.Println(windwest.Print())
+}
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"example/user/hello/business"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+	// Create three different energy offers of kineteco
+	solar2k := business.Solar{Name: "Solar 2000", Netto: 4.500}
+	solar3k := business.Solar{Name: "Solar 3000", Netto: 4.000}
+	windwest := business.Wind{Name: "Wind West", Netto: 3.950}
+
+	// Print details for each energy offer with kineteco branding
+	fmt.Println(solar3k.Print())
+	fmt.Println(solar2k.Print())
+	fmt.Println(windwest.Print())
+
+	fmt.Printf("Solar: %s\n", business.PrintGeneric[business.Solar](solar3k))
+	fmt.Printf("Wind: %s\n", business.PrintGeneric[business.Wind](windwest))
+
+	// Print functions for 01_04
+	ss := []business.Solar{solar2k, solar3k}
+	business.PrintSlice[business.Solar](ss)
+	business.PrintSlice[business.Wind]([]business.Wind{windwest, windwest})
+}
