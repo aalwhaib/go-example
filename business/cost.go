7b64fddd60e910bda2a402b6a781c2abe712cb3c
Ahmed Alwahib <aalwheep@gmail.com>
2024-01-26 22:42:13 +0300

    03_01

--- business/cost.go
+++ business/cost.go
@@ -1,9 +1,22 @@
 package business
 
+import "golang.org/x/exp/constraints"
+
 func (s Solar) Cost() float64 {
 	return s.Netto * 0.4
 }
 
 func (w Wind) Cost() float64 {
-	return w.Netto * 0.4
+	return w.Netto * 0.3
+}
+
+// Number is either a floating point number or an integer
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
+// Cost multiplies usage with netto to compute the cost.
+func Cost[T Number](usage, netto T) T {
+	cost := usage * netto
+	return cost
 }
--- main.go
+++ main.go
@@ -19,9 +19,16 @@ func main() {
 
 	fmt.Printf("Solar: %s\n", business.PrintGeneric[business.Solar](solar3k))
 	fmt.Printf("Wind: %s\n", business.PrintGeneric[business.Wind](windwest))
+	fmt.Printf("Solar: %s\n", business.PrintGeneric(solar2k))
+	fmt.Printf("Wind: %s\n", business.PrintGeneric(windwest))
 
 	// Print functions for 01_04
 	ss := []business.Solar{solar2k, solar3k}
 	business.PrintSlice[business.Solar](ss)
 	business.PrintSlice[business.Wind]([]business.Wind{windwest, windwest})
+
+	business.Cost(10, solar2k.Netto)
+	business.Cost(0.46, 10)
+	business.Cost(0.46, float64(10))
+	business.Cost[float64](0.46, 10)
 }
